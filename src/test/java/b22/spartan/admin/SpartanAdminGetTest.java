51997754d818e34898046e3ebdc8a093bd5000b6
ZalinaKabulova <105004544+ZalinaKabulova@users.noreply.github.com>
2022-12-12 13:12:17 +0100

    Day 13

---
+++ src/test/java/b22/spartan/admin/SpartanAdminGetTest.java
@@ -0,0 +1,43 @@
+package b22.spartan.admin;
+
+import io.restassured.http.ContentType;
+import org.checkerframework.checker.units.qual.C;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+
+public class SpartanAdminGetTest {
+
+    @BeforeAll
+    public static void init(){
+
+        //save baseurl inside this variable so that we dont need to type each http method
+        baseURI = "http://3.86.103.110:7000";
+
+
+    }
+
+    @Test
+    public void getAllSpartan(){
+
+        given()
+                .accept(ContentType.JSON)
+                .and()
+                .auth().basic("admin","admin")
+        .when()
+                .get("/api/spartans")
+        .then()
+                .statusCode(200)
+                .and()
+                .contentType(ContentType.JSON);
+
+
+
+
+
+    }
+
+
+}
